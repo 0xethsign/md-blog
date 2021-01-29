467d314d000bdcf49482cac855ee1e7b17eebce0
smrpn <samarpan_d@pp.iitr.ac.in>
2021-01-29 23:44:06 +0530

    frontend logics added

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+
+
+const Landing = () => (
+   <Container>
+      <h1> Blogging App</h1>
+   </Container>
+)
+
+export default Landing
---
+++ client/src/components/layout/NavigationBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import './Nav.css'
+const Navigationbar = ({ auth, onClick }) => (
+   <Navbar>
+
+      <ul>
+         <li><a href="/">Home</a></li>
+         <Nav className="ml-auto">
+            {auth ? (
+               <li><Link to="/logout">
+                  <Button
+                     onClick={onClick}>
+                     Logout
+               </Button>
+               </Link>
+               </li>
+            ) : (
+                  <li><Link to="/login">
+                     <Button>Login</Button></Link>
+                  </li>
+               )}
+         </Nav>
+      </ul>
+
+   </Navbar>
+)
+
+Navigationbar.propTypes = {
+   auth: PropTypes.bool.isRequired,
+   onClick: PropTypes.func.isRequired
+}
+
+export default Navigationbar
